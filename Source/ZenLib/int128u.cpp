6f8b98f7828387f5d264f70f849b762620a2e592
JeromeMartinez <jerome@mediaarea.net>
2009-09-04 06:56:34 +0000

    + Support of more platforms (fmodf)
    git-svn-id: svn://svn.code.sf.net/p/zenlib/code/ZenLib/trunk@175 f098ebc7-0e84-4588-8ad0-e2271d839f10

--- Source/ZenLib/int128u.cpp
+++ Source/ZenLib/int128u.cpp
@@ -44,7 +44,8 @@
 #ifdef __BORLANDC__
     #define fmodf fmod
 #endif
-#if defined (__MONTAVISTA__) || defined (__ARMEL__)     || \
+#if defined (__NO_LONG_DOUBLE_MATH) || \
+    defined (__MONTAVISTA__) || defined (__ARMEL__)     || \
     defined (__FreeBSD__)    || defined (__OpenBSD__)   || \
     defined (__NetBSD__)     || defined (__DragonFly__) || \
     defined (__sparc__)      || defined (__sparc64__)
