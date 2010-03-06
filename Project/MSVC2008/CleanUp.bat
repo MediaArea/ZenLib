c39ba767dd4a0b69771ee7f844f39509e671d291
JeromeMartinez <jerome@mediaarea.net>
2010-03-06 18:41:00 +0000

    + MSVC2010 project files
    git-svn-id: svn://svn.code.sf.net/p/zenlib/code/ZenLib/trunk@255 f098ebc7-0e84-4588-8ad0-e2271d839f10

--- Project/MSVC2008/CleanUp.bat
+++ Project/MSVC2008/CleanUp.bat
@@ -1,16 +1,15 @@
 @rem echo off
 
 @rem MS Visual Studio specific ---
-del *.opensdf *.sdf
 cd Example
 rmdir Win32 /Q /S
 rmdir x64/Q /S
-del *.ncb *.user *.filters
+del *.ncb *.user
 del *.suo /AH
 cd ..
 cd Library
 rmdir Win32 /Q /S
 rmdir x64/Q /S
-del *.ncb *.user *.filters
+del *.ncb *.user
 del *.suo /AH
 cd ..
