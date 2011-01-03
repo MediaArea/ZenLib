0818cee897da01670c686ab3c29e8ed4d308635b
JeromeMartinez <jerome@mediaarea.net>
2011-01-03 18:56:32 +0000

    + Copyright date change
    git-svn-id: svn://svn.code.sf.net/p/zenlib/code/ZenLib/trunk@308 f098ebc7-0e84-4588-8ad0-e2271d839f10

--- Source/ZenLib/Ztring.h
+++ Source/ZenLib/Ztring.h
@@ -1,5 +1,5 @@
 // ZenLib::Ztring - More methods for std::(w)string
-// Copyright (C) 2002-2010 MediaArea.net SARL, Info@MediaArea.net
+// Copyright (C) 2002-2011 MediaArea.net SARL, Info@MediaArea.net
 //
 // This software is provided 'as-is', without any express or implied
 // warranty.  In no event will the authors be held liable for any damages
