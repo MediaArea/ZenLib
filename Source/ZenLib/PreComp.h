b0c91ce910cd3a6cea4a1891c7bbb036f4a363aa
JeromeMartinez <jerome@mediaarea.net>
2011-08-16 07:49:57 +0000

    + Precompiled headers for MSVC, based on a patch by XhmikosR
    git-svn-id: svn://svn.code.sf.net/p/zenlib/code/ZenLib/trunk@333 f098ebc7-0e84-4588-8ad0-e2271d839f10

---
+++ Source/ZenLib/PreComp.h
@@ -0,0 +1,45 @@
+// PreComp - PreComp file for ZenLib
+// Copyright (C) 2006-2011 MediaArea.net SARL, Info@MediaArea.net
+//
+// This library is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this library. If not, see <http://www.gnu.org/licenses/>.
+//
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//
+// Helpers for compilers (precompilation)
+//
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//---------------------------------------------------------------------------
+#ifndef MediaInfo_PreCompH
+#define MediaInfo_PreCompH
+//---------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------
+#if defined(_MSC_VER) || defined(__BORLANDC__)
+    #include <cstring>
+    #include <cstdio>
+    #include <cstdlib>
+    #include <ctime>
+    #include <algorithm>
+    #include <map>
+    #include <sstream>
+    #include <iomanip>
+    #include <cmath>
+    #include "ZenLib/Conf.h"
+    #include "ZenLib/Conf_Internal.h"
+#endif //defined(_MSC_VER) || defined(__BORLANDC__)
+//---------------------------------------------------------------------------
+
+#endif
