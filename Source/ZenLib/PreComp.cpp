ad2947d0c9ceb6705037b454c1b53a42d15fe88e
JeromeMartinez <jerome@mediaarea.net>
2012-01-21 21:42:09 +0000

    x Error in the license
    git-svn-id: svn://svn.code.sf.net/p/zenlib/code/ZenLib/trunk@367 f098ebc7-0e84-4588-8ad0-e2271d839f10

--- Source/ZenLib/PreComp.cpp
+++ Source/ZenLib/PreComp.cpp
@@ -1,25 +1,23 @@
-// PreComp - PreComp file for ZenLib
-// Copyright (C) 2006-2011 MediaArea.net SARL, Info@MediaArea.net
+// ZenLib::Translation - Helper for translation
+// Copyright (C) 2007-2011 MediaArea.net SARL, Info@MediaArea.net
 //
-// This library is free software: you can redistribute it and/or modify it
-// under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// any later version.
+// This software is provided 'as-is', without any express or implied
+// warranty.  In no event will the authors be held liable for any damages
+// arising from the use of this software.
 //
-// This library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Lesser General Public License for more details.
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
 //
-// You should have received a copy of the GNU Lesser General Public License
-// along with this library. If not, see <http://www.gnu.org/licenses/>.
+// 1. The origin of this software must not be misrepresented; you must not
+//    claim that you wrote the original software. If you use this software
+//    in a product, an acknowledgment in the product documentation would be
+//    appreciated but is not required.
+// 2. Altered source versions must be plainly marked as such, and must not be
+//    misrepresented as being the original software.
+// 3. This notice may not be removed or altered from any source distribution.
 //
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-//
-// Helpers for compilers (precompilation)
-//
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 //---------------------------------------------------------------------------
 #include "ZenLib/PreComp.h"
