2231d3669ab69dc6bbbe365f10af8373dc6f2d0c
JeromeMartinez <jerome@mediaarea.net>
2012-08-08 09:42:43 +0000

    Changing Copyright date
    git-svn-id: svn://svn.code.sf.net/p/zenlib/code/ZenLib/trunk@415 f098ebc7-0e84-4588-8ad0-e2271d839f10

--- Source/ZenLib/Format/Http/Http_Handler.cpp
+++ Source/ZenLib/Format/Http/Http_Handler.cpp
@@ -1,5 +1,5 @@
 // ZenLib::Format::Http::Handler - A HTTP Handler
-// Copyright (C) 2008-2011 MediaArea.net SARL, Info@MediaArea.net
+// Copyright (C) 2008-2012 MediaArea.net SARL, Info@MediaArea.net
 //
 // This software is provided 'as-is', without any express or implied
 // warranty.  In no event will the authors be held liable for any damages
